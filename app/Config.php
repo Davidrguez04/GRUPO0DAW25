ce9b62e4ed757e81c1ff8edc8dc9de93f8f90c73
Davidrguez04 <122359982+Davidrguez04@users.noreply.github.com>
2026-02-24 16:33:59 +0100

    Update Config.php

--- app/Config.php
+++ app/Config.php
@@ -4,8 +4,8 @@
  {
      static public $mvc_bd_hostname = "localhost";
      static public $mvc_bd_nombre   = "alimentos2025";
-     static public $mvc_bd_usuario  = "danielu";
-     static public $mvc_bd_clave    = "Q1GuQDIX";
+     static public $mvc_bd_usuario = "myalimentobc";
+static public $mvc_bd_clave = "Alimentos2025!";
      static public $mvc_vis_css     = "estilo.css";
  }
 ?>
